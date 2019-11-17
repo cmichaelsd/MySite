4a92f0bf5d0b0deeb92486ad667794b8d644bda6
Cole Michaels <cmichaelsd@gmail.com>
2019-11-17 01:06:23 -0800

    Added larger profile photo.

--- pages/education.tsx
+++ pages/education.tsx
@@ -10,12 +10,12 @@ const Education: NextPage = () => {
                 <title>Coles Michaels - Education</title>
             </Head>
             <div className="education-container">
-                <BannerImage href="https://www.hackreactor.com/" src="/static/fallbacks/HackReactor.png" alt="Hack Reactor Logo" />
+                <BannerImage href="https://www.hackreactor.com/" src="/static/HackReactor.png" alt="Hack Reactor Logo" />
                 <p className="margin">Hack Reactor is a Fullstrack Wed Development bootcamp covering topics such as Data Structures and Memory, Databases, Microservice Architecture, Frontend Design and CSS, and Program Efficiency.</p>
                 <p className="margin">I was accepted into Hack Reactor's 21st cohort March of 2018, each cohort lasts three months and each student is required to be on campus 12 hours a day 6 days a week. This is a full immersion bootcamp where students have their eduaction equated to drinking from a firehose.</p>
             </div>
             <div className="education-container">
-                <BannerImage href="https://www.trilogyed.com/" src="/static/fallbacks/TrilogyEducationServices.png" alt="Trilogy Education Logo" />
+                <BannerImage href="https://www.trilogyed.com/" src="/static/TrilogyEducationServices.png" alt="Trilogy Education Logo" />
                 <p className="margin">Trilogy Education Services is a national program which plugs into state universities, they offer education in Fullstack Web Devlopment and offer in-person and remote tutoring outside of class.</p>
                 <p className="margin">After graduating Hack Reactor I moved on to become a tutor at Trilogy Education Services, where I worked one-on-one with a minimum of 10 students a week offering support in a wide range of topics like CSS fundamentals, jQuery, React, MongoDB, SQL, JavaScript, Python, and Deployment.</p>
             </div>
