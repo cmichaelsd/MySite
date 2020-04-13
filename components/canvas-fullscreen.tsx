6c8e482fffd9a25cf6514d967c801d340dc904c0
Cole Michaels <cmichaelsd@gmail.com>
2020-04-12 23:11:54 -0700

    fix canvas: memory leak with animation function, reassign as an empty function.

--- components/canvas-fullscreen.tsx
+++ components/canvas-fullscreen.tsx
@@ -55,15 +55,35 @@ export default class CanvasFullScreen extends Component {
         } : null;
     };
 
+    animate = (length: number, amplitude: number, increment: number): void => {
+        let bg: any = this.hexToRgb(this.context.theme.toneOne);
+        requestAnimationFrame(() => this.animate(length, amplitude, increment));
+
+        this.c.fillStyle = `rgba(${bg.r}, ${bg.g}, ${bg.b}, 0.01)`;
+
+        this.c.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.c.beginPath();
+        this.c.moveTo(0, this.canvas.height / 2);
+        if (this.wave) {
+            for (let i: number = 0; i < this.canvas.width; i++) {
+                this.c.lineTo(i, this.wave.y + Math.sin(i * this.wave.length + increment) * (this.wave.amplitude * Math.sin(increment)));
+            }
+            for (let j: number = this.canvas.width; j > 0 ; j--) {
+                this.c.lineTo(j, this.wave.y - Math.sin(j * this.wave.length + increment) * (this.wave.amplitude * Math.sin(increment)));
+            }
+        }
+        this.c.strokeStyle = `hsl(${Math.abs(this.strokeColor.h * Math.sin(increment))}, ${this.strokeColor.s}%, ${this.strokeColor.l}%)`;
+        this.c.stroke();
+        if (this.wave) {
+            increment += this.wave.frequency;
+        }
+    };
+
     componentDidMount = (): void => {
         this.canvas = this.refs.canvas;
         this.c = this.canvas.getContext('2d');
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight + this.offsetYOverflow;
-        // wave length
-        // default: length: 0.008 amp: 260
-        // width 968: length: 0.005 amp: 360
-        // width 1814: length: 0.010 amp: 260
         let length = 0.008;
         let amplitude = 260;
         if (this.canvas.width >= 1814) {
@@ -83,35 +103,15 @@ export default class CanvasFullScreen extends Component {
 
         window.addEventListener('resize', this.handleResize);
 
-        const animate = (): void => {
-            let bg: any = this.hexToRgb(this.context.theme.toneOne);
-            requestAnimationFrame(animate);
-
-            this.c.fillStyle = `rgba(${bg.r}, ${bg.g}, ${bg.b}, 0.01)`;
-
-            this.c.fillRect(0, 0, this.canvas.width, this.canvas.height);
-            this.c.beginPath();
-            this.c.moveTo(0, this.canvas.height / 2);
-            if (this.wave) {
-                for (let i: number = 0; i < this.canvas.width; i++) {
-                    this.c.lineTo(i, this.wave.y + Math.sin(i * this.wave.length + increment) * (this.wave.amplitude * Math.sin(increment)));
-                }
-                for (let j: number = this.canvas.width; j > 0 ; j--) {
-                    this.c.lineTo(j, this.wave.y - Math.sin(j * this.wave.length + increment) * (this.wave.amplitude * Math.sin(increment)));
-                }
-            }
-            this.c.strokeStyle = `hsl(${Math.abs(this.strokeColor.h * Math.sin(increment))}, ${this.strokeColor.s}%, ${this.strokeColor.l}%)`;
-            this.c.stroke();
-            if (this.wave) {
-                increment += this.wave.frequency;
-            }
-        }
-        animate();
-    }
+        this.animate(length, amplitude, increment);
+    };
 
     componentWillUnmount = (): void => {
-        window.removeEventListener('resize', this.handleResize);
-    }
+        window.removeEventListener('resize', this.handleResize);   
+        this.animate = (length: number, amplitude: number, increment: number): void => {
+            return;
+        }
+    };
 
     render() {
         let bg: any = this.hexToRgb(this.context.theme.toneOne);
