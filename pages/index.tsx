5692b7921a96e2efc5f04adeb11321d7146ff5eb
Cole Michaels <cmichaelsd@gmail.com>
2020-04-25 19:53:52 -0700

    refactor index: updated job information.

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default class Index extends Component {
                         <p>I'm Cole Michaels.</p>
                         <p>
                             I'm a fulltime Fullstack Engineer for V Shred.
-                            I was formerly a oncall as a Fullstack Engineer for Jijenge Academy and a Software Engineer Tutor with Trilogy Education Services at UCLA supporting a minimum of 10 students a week in specific technologies within Fullstack Web Development.
+                            I was formerly oncall as a Fullstack Engineer for Jijenge Academy and a Software Engineer Tutor with Trilogy Education Services at UCLA supporting a minimum of 10 students a week in specific technologies within Fullstack Web Development.
                         </p>
                         <p>
                             In March of 2018 I was accepted into Hack Reactor, Los Angeles' 21st cohort. While at Hack Reactor I studied Web Development and gained the skills I use today in my career and met most of my peers within the Los Angeles WebDev community.
