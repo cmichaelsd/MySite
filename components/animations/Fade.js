594d5d54d575a7c1042d96cb3ff26e1333e680fa
Cole Michaels <cmichaelsd@gmail.com>
2021-02-08 13:42:21 -0800

    removed comments in fade.

--- components/animations/Fade.js
+++ components/animations/Fade.js
@@ -1,20 +1,5 @@
 import { useState, useEffect } from 'react'
 
-// TODO: Set for fadeIn and/or fadeOut animation
-/**
- * When show is true an animation is triggered for fadeIn.
- * At some point in time if show is changed in the parent the local shouldRender is set to false.
- * Once false a fadeOut animation is triggered.
- * 
- * What I want:
- * Fade In - 
- * When fadeIn is true an animation is triggered for fadeIn.
- * 
- * Fade Out - 
- * When fadeOut is true no animation is set for initial render.
- * At some point in time if fadeOut becomes false in parent.
- * @param {any} param0
- */
 function Fade({ show, fadeIn = true, fadeOut = true, time = 2, children }) {
     const [shouldRender, setShouldRender] = useState(show);
 
@@ -44,29 +29,5 @@ function Fade({ show, fadeIn = true, fadeOut = true, time = 2, children }) {
     )
 
 }
-
-/*
-function Fade({ show, children }) {
-    const [shouldRender, setShouldRender] = useState(show);
-
-    const onAnimationEnd = () => {
-        if (!show) setShouldRender(false);
-    }
-
-    useEffect(() => {
-        if (show) setShouldRender(true);
-    }, [show]) // cb runs only if 'show' is updated.
-
-    return shouldRender && (
-        <div
-            style={{ animation: `${show ? "fadeIn" : "fadeOut"} 2s` }}
-            onAnimationEnd={onAnimationEnd}
-        >
-            {children}
-        </div>
-    )
-
-}
-*/
 
 export default Fade
