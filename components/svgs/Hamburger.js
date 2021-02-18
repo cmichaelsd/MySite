5011faa854c6cc006320087e57c2af88773dd684
Cole Michaels <cmichaelsd@gmail.com>
2021-02-18 12:41:18 -0800

    added aira-label to hamburger menu icon.

--- components/svgs/Hamburger.js
+++ components/svgs/Hamburger.js
@@ -9,7 +9,7 @@ function Hamburger() {
     const { changeStatus } = useContext(ApplicationStatusContext);
 
     return (
-        <SVGButton svgButtonAction={() => changeStatus(Events.NAVIGATION_DRAWER_OPENED) }>
+        <SVGButton svgButtonAction={() => changeStatus(Events.NAVIGATION_DRAWER_OPENED) } label="Open Side Navigation">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16">
                 <path fill={theme.textColor} d="M1 3h14v3h-14zM1 7h14v3h-14zM1 11h14v3h-14z"></path>
             </svg>
